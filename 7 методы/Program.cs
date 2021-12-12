0a02be2ab7ac422335574bd820da331d8e000db1
User <User@SERGEY>
2021-12-12 15:53:37 +0300

    Добавьте файлы проекта.

---
+++ 7 методы/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длины сторон ПЕРВОГО треугольника");
+            int a1 = Convert.ToInt32(Console.ReadLine());
+            int b1 = Convert.ToInt32(Console.ReadLine());
+            int c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длины сторон ВТОРОГО треугольника");
+            int a2 = Convert.ToInt32(Console.ReadLine());
+            int b2 = Convert.ToInt32(Console.ReadLine());
+            int c2 = Convert.ToInt32(Console.ReadLine());
+            comptriangsqr(a1, b1, c1, a2, b2, c2);
+            Console.ReadKey();
+        }
+        static void comptriangsqr(int a1, int b1, int c1, int a2, int b2, int c2)
+        {
+            double P1 = a1 + b1 + c1;
+            double P2 = a2 + b2 + c2;
+            double S1 = Math.Sqrt(P1 * (P1-a1) * (P1-b1) * (P1-c1));
+            double S2 = Math.Sqrt(P1 * (P2 - a2) * (P2 - b2) * (P2 - c2));
+            if (S1 > S2)
+            {
+                Console.WriteLine("Площадь ПЕРВОГО треугольника БОЛЬШЕ чем ВТОРОГО");
+            }
+            else
+                        if (S1 < S2)
+            {
+                Console.WriteLine("Площадь ВТОРОГО треугольника БОЛЬШЕ чем ПЕРВОГО");
+            }
+            else
+            { 
+                Console.WriteLine("Площади ПЕРВОГО и ВТОРОГО треугольников равны");
+            }
+        }
+    }
+}
---
+++ vtj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vtj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны куба");
+            int a = Convert.ToInt32(Console.ReadLine());
+            sqrandvol(a);
+            Console.ReadKey();
+        }
+        static void sqrandvol(int a)
+        {
+            int S = 6*a*a;
+            int V = a * a * a;
+                Console.Write("Площадь куба = {0}",S);
+            Console.WriteLine("м2");
+            Console.Write("Объем куба = {0}", V);
+            Console.WriteLine("м3");
+        }
+            
+    }
+}
